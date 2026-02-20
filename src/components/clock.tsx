7c8de3ee947d278bc218d638fa83779f462852fe
8bitbug <akramo@tuta.io>
2026-02-20 22:29:53 +0000

    Clock face background

--- src/components/clock.tsx
+++ src/components/clock.tsx
@@ -1,95 +1,144 @@
 import { onMount, type Component, createEffect } from "solid-js";
 import "../App.css";
 import { game } from "./game";
+import { opacity, palette } from "./palette";
 
 interface ClockProps {
-    height: number,
-    width: number,
+  height: number;
+  width: number;
 }
 
 const Clock: Component<ClockProps> = (props) => {
-    let canvasRef: HTMLCanvasElement | undefined;
+  let canvasRef: HTMLCanvasElement | undefined;
 
-    onMount(() => {
-        if (!canvasRef) { return; }
+  onMount(() => {
+    if (!canvasRef) {
+      return;
+    }
 
-        const canvas = canvasRef;
-        const ctx = canvas.getContext("2d");
-        if (!ctx) { return; }
+    const canvas = canvasRef;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) {
+      return;
+    }
 
-        ctx.fillStyle = "white";
-        ctx.strokeStyle = "white";
+    let radius = canvas.height / 2;
+    ctx.translate(radius, radius);
+    radius *= 0.9;
 
-        let radius = canvas.height / 2;
-        ctx.translate(radius, radius);
-        radius *= 0.90;
+    createEffect(() => {
+      ctx.save();
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.restore();
 
-        createEffect(() => {
-            ctx.save();
-            ctx.setTransform(1, 0, 0, 1, 0, 0);
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.restore();
+      ctx.beginPath();
+      ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+      ctx.fillStyle = palette.face;
+      ctx.fill();
 
-            ctx.beginPath();
-            ctx.lineWidth = 3;
-            ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-            ctx.stroke();
+      ctx.save();
+      ctx.beginPath();
+      ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+      ctx.clip();
 
-            ctx.beginPath();
-            ctx.arc(0, 0, radius / 20, 0, 2 * Math.PI);
-            ctx.fill();
+      // dark overlay, the main part of the clock face background
+      ctx.beginPath();
+      ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+      ctx.fillStyle = "#000000" + opacity(50);
+      ctx.fill();
 
-            ctx.font = radius * 0.15 + "px arial";
-            ctx.textBaseline = "middle";
-            ctx.textAlign = "center";
-            for (let num = 1; num < 13; num++) {
-                let ang = num * Math.PI / 6;
-                ctx.rotate(ang);
-                ctx.translate(0, -radius * 0.85);
-                ctx.rotate(-ang);
-                ctx.fillText(num.toString(), 0, 0);
-                ctx.rotate(ang);
-                ctx.translate(0, radius * 0.85);
-                ctx.rotate(-ang);
-            }
+      // faded cutout
+      // we make this minus and that makes the bright purple circle appear on top left instead of bottom right
+      const offset = radius * 0.35;
 
-            const drawHand = (pos: number, length: number, width: number) => {
-                ctx.beginPath();
-                ctx.lineWidth = width;
-                ctx.lineCap = "round";
-                ctx.moveTo(0, 0);
-                ctx.rotate(pos);
-                ctx.lineTo(0, -length);
-                ctx.stroke();
-                ctx.rotate(-pos);
-            }
+      const grd = ctx.createRadialGradient(
+        -offset,
+        -offset,
+        0,
+        -offset,
+        -offset,
+        radius * 1.2
+      );
+      grd.addColorStop(0, "#1a1020");
+      grd.addColorStop(1, "#1a1020" + opacity(0));
 
-            let hour = (game.time / 720) % 60;
-            let minute = (game.time / 60) % 60;
-            let second = game.time % 60;
+      ctx.beginPath();
+      ctx.arc(-offset, -offset, radius * 1.2, 0, Math.PI * 2);
+      ctx.fillStyle = grd;
+      ctx.fill();
 
-            hour = (hour * Math.PI / 30);
-            minute = (minute * Math.PI / 30);
-            second = (second * Math.PI / 30);
+      ctx.restore();
 
-            drawHand(hour, radius * 0.45, radius * 0.09);
-            drawHand(minute, radius * 0.75, radius * 0.05);
-            drawHand(second, radius * 0.9, radius * 0.02);
-        });
+      ctx.beginPath();
+      ctx.lineWidth = 1;
+      ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+      ctx.strokeStyle = palette.faint;
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.arc(0, 0, radius / 20, 0, 2 * Math.PI);
+      ctx.fillStyle = palette.highlight;
+      ctx.fill();
+
+      ctx.fillStyle = palette.dim;
+      ctx.font = radius * 0.15 + "px arial";
+      ctx.textBaseline = "middle";
+      ctx.textAlign = "center";
+      for (let num = 1; num < 13; num++) {
+        let ang = (num * Math.PI) / 6;
+        ctx.rotate(ang);
+        ctx.translate(0, -radius * 0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius * 0.85);
+        ctx.rotate(-ang);
+      }
+
+      const drawHand = (
+        pos: number,
+        length: number,
+        width: number,
+        color: string
+      ) => {
+        ctx.beginPath();
+        ctx.lineWidth = width;
+        ctx.lineCap = "round";
+        ctx.strokeStyle = color;
+        ctx.moveTo(0, 0);
+        ctx.rotate(pos);
+        ctx.lineTo(0, -length);
+        ctx.stroke();
+        ctx.rotate(-pos);
+      };
+
+      let hour = (game.time / 720) % 60;
+      let minute = (game.time / 60) % 60;
+      let second = game.time % 60;
+
+      hour = (hour * Math.PI) / 30;
+      minute = (minute * Math.PI) / 30;
+      second = (second * Math.PI) / 30;
+
+      drawHand(hour, radius * 0.45, radius * 0.09, palette.highlight);
+      drawHand(minute, radius * 0.75, radius * 0.05, palette.text);
+      drawHand(second, radius * 0.9, radius * 0.02, palette.dim);
     });
+  });
 
-    return (
-        <>
-            <div class="clock__time">{game.format(game.time)}</div>
-            <div class="clock__tps">{game.format(game.tps)}</div>
-            <canvas
-                class="clock"
-                ref={canvasRef}
-                height={props.height}
-                width={props.width}
-            />
-        </>
-    );
-}
+  return (
+    <>
+      <div class="clock__time">{game.format(game.time)}</div>
+      <div class="clock__tps">{game.format(game.tps)}</div>
+      <canvas
+        class="clock"
+        ref={canvasRef}
+        height={props.height}
+        width={props.width}
+      />
+    </>
+  );
+};
 
 export default Clock;
---
+++ src/components/palette.ts
@@ -0,0 +1,14 @@
+export const palette = {
+  highlight: "#f5d9a0", // warm star gold, main highlights, numbers
+  text: "#e8dcc8", // soft off-white, body text
+  dim: "#8a7a68", // secondary text
+  faint: "#4a3d30", // labels, hints
+  face: "#0d0a12", // background
+};
+
+export function opacity(percent: number): string {
+  const hex = Math.round(percent * 2.55)
+    .toString(16)
+    .padStart(2, "0");
+  return hex;
+}
